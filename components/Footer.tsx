96fdee124edec5934039b58b7697f77c73a65494
JacoLombardo <jacopo.lombardo@outlook.com>
2023-11-08 11:17:59 +0100

    image next.js fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import styles from "@/styles/general.module.css";
+import Image from "next/image";
 
 export default function Footer() {
   return (
@@ -16,23 +17,92 @@ export default function Footer() {
             <a
               href="https://github.com/JacoLombardo"
               target="_blank"
-              className={styles.github}
+              className={styles.github_link}
             >
-              <div></div>
+              <Image
+                src={
+                  "https://res.cloudinary.com/dtl48kr1u/image/upload/v1699266007/bad-jokes/Icons/github-hover_gl53oh.png"
+                }
+                title={"github"}
+                alt={"github"}
+                width={50}
+                height={50}
+                sizes="100vw"
+                priority={true}
+                className={styles.github}
+              />
+              <Image
+                src={
+                  "https://res.cloudinary.com/dtl48kr1u/image/upload/v1699266007/bad-jokes/Icons/github_tmtdhi.png"
+                }
+                title={"github"}
+                alt={"github"}
+                width={50}
+                height={50}
+                sizes="100vw"
+                priority={true}
+                className={styles.github_on}
+              />
             </a>
             <a
               href="https://www.linkedin.com/in/jacopo-lombardo/"
               target="_blank"
-              className={styles.linkedin}
+              className={styles.linkedin_link}
             >
-              <div></div>
+              <Image
+                src={
+                  "https://res.cloudinary.com/dtl48kr1u/image/upload/v1699266007/bad-jokes/Icons/linkedin-hover_dbf5jc.png"
+                }
+                title={"linkedin"}
+                alt={"linkedin"}
+                width={50}
+                height={50}
+                sizes="100vw"
+                priority={true}
+                className={styles.linkedin}
+              />
+              <Image
+                src={
+                  "https://res.cloudinary.com/dtl48kr1u/image/upload/v1699266007/bad-jokes/Icons/linkedin_nnkfyz.png"
+                }
+                title={"linkedin"}
+                alt={"linkedin"}
+                width={50}
+                height={50}
+                sizes="100vw"
+                priority={true}
+                className={styles.linkedin_on}
+              />
             </a>
             <a
               href="mailto:jacopo.lombardo@outlook.com"
               target="_blank"
-              className={styles.mail}
+              className={styles.mail_link}
             >
-              <div></div>
+              <Image
+                src={
+                  "https://res.cloudinary.com/dtl48kr1u/image/upload/v1699266007/bad-jokes/Icons/speech-bubble_klrxpe.png"
+                }
+                title={"mail"}
+                alt={"mail"}
+                width={50}
+                height={50}
+                sizes="100vw"
+                priority={true}
+                className={styles.mail}
+              />
+              <Image
+                src={
+                  "https://res.cloudinary.com/dtl48kr1u/image/upload/v1699266007/bad-jokes/Icons/speech-bubble-hover_xjyjkx.png"
+                }
+                title={"mail"}
+                alt={"mail"}
+                width={50}
+                height={50}
+                sizes="100vw"
+                priority={true}
+                className={styles.mail_on}
+              />
             </a>
           </div>
         </div>
