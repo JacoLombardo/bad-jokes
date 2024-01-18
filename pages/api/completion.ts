da5d3ba2d95313b5d72e7d6a34cf95391346e0e7
JacoLombardo <jacopo.lombardo@outlook.com>
2024-01-18 11:09:19 +0100

    fix deprecation

--- pages/api/completion.ts
+++ pages/api/completion.ts
@@ -16,7 +16,7 @@ export default async function completion(
   }
 
   const response = await openai.completions.create({
-    model: "text-davinci-003",
+    model: "gpt-3.5-turbo-instruct",
     prompt: prompt,
     max_tokens: 50,
   });
